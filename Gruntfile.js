7f5729ee2c09b37f35c86a6e899196b7531b94a3
Vasiliy Kirstia <vasiliykirsia@yandex.com>
2016-08-31 17:06:20 +0300

    day end

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,15 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                // the banner is inserted at the top of the output
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-            },
-            dist: {
-                files: {
-                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-                }
-            }
-        },
-        jshint: {
-            // define the files to lint
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-            // configure JSHint (documented at http://www.jshint.com/docs/)
-            options: {
-                // more options here if you want to override JSHint defaults
-                globals: {
-                    jQuery: true,
-                    console: true,
-                    module: true
-                }
+        jade:{
+            compile:{
+                src: 'src/frontend/**/*.jade',
+                dest: 'dist/frontend/'
             }
         }
     });
+
+    grunt.loadNpmTasks('grunt-contrib-jade');
+
+    grunt.registerTask('jade', 'jade')
 };
