138de0910e6fd2eab62d2b497f5b987dd6fa8b72
Vasiliy Kirstia <vasiliykirstia@yandex.ru>
2016-09-05 12:22:23 +0300

    Начал проходить туториал по angular.

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,14 +45,23 @@ module.exports = function(grunt) {
                 src: "bower_components/**/*",
                 dest: "dist/frontend"
             }
+        },
+        clean: {
+            frontend: {
+                expand: true,
+                cwd: "dist/frontend",
+                src: "**/*"
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
+    grunt.registerTask('clean-all', "Clean frontend and backend directories.", ['clean:frontend']);
     grunt.registerTask('build-backend', "Copy backend source code.", ['copy:backend']);
     grunt.registerTask('build-frontend', "Prepare frontend source code.", ['jade:compile', 'htmlmin:frontend', 'copy:frontend']);
-    grunt.registerTask('default','Build frontend and backend.', ['build-frontend', 'build-backend']);
+    grunt.registerTask('default','Build frontend and backend.', ['clean-all', 'build-frontend', 'build-backend']);
 };
