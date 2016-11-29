e9a6d26522f711af54b6acc412127db746bdeff9
VasiliyKirstia <vailiykirstia@yandex.ru>
2016-11-29 15:14:03 +0200

    добавил верхнюю менюшку и перешел с less на sass

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -15,6 +15,21 @@ module.exports = function(grunt) {
             }
         },
 
+        sass: {
+            compile: {
+                options: {
+                    style: 'expanded'
+                },
+                files: [ {
+                    cwd: "src/styles",
+                    src: "**/*.sass",
+                    dest: "dist/styles",
+                    expand: true,
+                    ext: ".css"
+                } ]
+            }
+        },
+
         pug: {
             compile: {
                 options: {
@@ -34,11 +49,11 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-pug');
 
     grunt.registerTask('clean-all', 'Cleaning all distribution directories.', ['clean']);
-    grunt.registerTask('compile-styles', 'Compiling styles.', ['less:compile']);
+    grunt.registerTask('compile-styles', 'Compiling styles.', ['sass:compile']);
     grunt.registerTask('compile-templates', 'Compiling templates.', ['pug:compile']);
 
     grunt.registerTask('default','Build frontend.', ['clean-all', 'compile-styles', 'compile-templates']);
